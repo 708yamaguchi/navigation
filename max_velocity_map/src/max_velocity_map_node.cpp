02145fb2fc97934589ab8ac0d4f1b04043fa7daf
Naoya Yamaguchi <708yamaguchi@gmail.com>
2019-07-19 21:54:59 +0900

    separate main cpp file into header and cpp

--- max_velocity_map/src/max_velocity_map_node.cpp
+++ max_velocity_map/src/max_velocity_map_node.cpp
@@ -1,57 +1,7 @@
-#include <ros/ros.h>
-#include <std_msgs/Float32.h>
-#include <geometry_msgs/PoseWithCovarianceStamped.h>
-#include <nav_msgs/OccupancyGrid.h>
-#include <amcl/map/map.h>
-
-#include <dynamic_reconfigure/DoubleParameter.h>
-#include <dynamic_reconfigure/Reconfigure.h>
-#include <dynamic_reconfigure/Config.h>
-
+#include <max_velocity_map/max_velocity_map.h>
 #include <algorithm>
-
 #include <signal.h>
 
-class MaxVelocityMap
-{
-  public:
-    MaxVelocityMap();
-    ~MaxVelocityMap();
-
-  private:
-    ros::NodeHandle nh_;
-    ros::Subscriber amcl_sub_;
-    ros::Subscriber map_sub_;
-    ros::Publisher max_vel_x_pub_;
-    ros::Publisher min_vel_x_pub_;
-    ros::Publisher max_vel_y_pub_;
-    ros::Publisher min_vel_y_pub_;
-    map_t* map_;
-    dynamic_reconfigure::ReconfigureRequest srv_req_;
-    dynamic_reconfigure::ReconfigureResponse srv_resp_;
-    dynamic_reconfigure::DoubleParameter double_param_;
-    dynamic_reconfigure::Config conf_;
-    std::string local_planner_;
-    float max_vel_x_initial_;
-    float min_vel_x_initial_;
-    float max_vel_y_initial_;
-    float min_vel_y_initial_;
-    // float max_vel_theta_initial_;
-    // float min_vel_theta_initial_;
-    void freeMapDependentMemory();
-    void amclCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg);
-    void mapReceived(const nav_msgs::OccupancyGridConstPtr& msg);
-    void handleMapMessage(const nav_msgs::OccupancyGrid& msg);
-    map_t* convertMap (const nav_msgs::OccupancyGrid& map_msg);
-};
-
-MaxVelocityMap* mvm;
-
-void reset_params(int sig)
-{
-  delete mvm;
-  ros::shutdown();
-}
 
 int main(int argc, char **argv)
 {
@@ -202,8 +152,7 @@ void MaxVelocityMap::handleMapMessage(const nav_msgs::OccupancyGrid& msg)
   map_ = convertMap(msg);
 }
 
-map_t*
-MaxVelocityMap::convertMap( const nav_msgs::OccupancyGrid& map_msg )
+map_t* MaxVelocityMap::convertMap( const nav_msgs::OccupancyGrid& map_msg )
 {
   map_t* map = map_alloc();
   ROS_ASSERT(map);
@@ -224,4 +173,10 @@ MaxVelocityMap::convertMap( const nav_msgs::OccupancyGrid& map_msg )
   }
 
   return map;
+}
+
+void reset_params(int sig)
+{
+  delete mvm;
+  ros::shutdown();
 }
